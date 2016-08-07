c9ec2249eeccffbd21c8bec3566d47533b6fed9a
Tej Pit <tejas2kxv@gmail.com>
2016-08-08 00:50:09 +1000

    updated_v
    updated the code with new function for taxes and super calculation. still needs fixing on inheritance of variables prompting repetition of the user input for var x.

--- salary_calc.py
+++ salary_calc.py
@@ -22,14 +22,59 @@ def overTime(hwage, regular_pay):
     print("The total overtime pay this week is: " ,otpay )
     sum = otpay + regular_pay 
     print("So total pay due this week is: ", sum)
-        
+    super_pay = float((9.5/100)*sum)
+    print ("Your super contribution this week is:",super_pay)
+    return super_pay 
+    
+def taxpay():
+#Computes the taxes for different income thresholds, for resident Aussies.
+    x = float(input("Enter the amount of your yearly income: "))
+    return x 
+    
+    while True:
+        total = 0 
+        if x  < 18200:
+            print("Congrats! You dont have to pay any tax! :)")
+            break 
+        elif 18201 < x < 37000:
+            total = ((x-18200)*0.19)
+            print ("You have to pay $", total , "in taxes this year")
+            break 
+        elif 37001 < x < 80000:
+            total = 3572 + ((x-37000)*0.32)
+            print("You have to pay $",(((x-37000)*0.32) +3572),"in taxes this year")
+            break 
+        elif 80001 < x < 180000:
+            total = 17547+((x-80000)*0.37)
+            print ("You have to pay $",total ,"in taxes this year")
+            break 
+        elif 180001 < x:
+            total = 54547+((x-180000)*0.45)
+            print ("You have to pay $",total ,"in taxes this year")
+            break
+        else:
+            print ("Invalid input. Enter again please.")
+            break
+                    
+def super(x):
+#Computes super over a gross income at a rate of 9.5%
+    super_rate = float(9.5/100)
+    super_gross = float(super_rate*(x))
+    print ("Your super contribution this year is: ",super_gross)
+    
+            
 def main():
 #Main function to pass vars from regPay to overTime and call.
     hw , r_p = regPay()
     overTime(hw, r_p)
+    taxpay()
+    y = taxpay()
+    super(y)
         
 #Call main
 main()
+
+
 
 
             
